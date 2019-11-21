6daac2514e34f3e47c332bb31041b26ca9684488
Mbugua KABAGE <mkabage@hotmail.com>
2019-11-21 15:24:10 +0530

    Added logic to capture fake session in browser

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -38,7 +38,19 @@
                         password: this.password
                     })
                     .then(response => {
- 
+                        localStorage.setItem('user',JSON.stringify('user'))
+                        localStorage.setItem('jwt','token')
+
+                        if (localStorage.getItem('jwt') != null){
+                            this.$emit('loggedIn')
+                            if(this.$route.params.nextUrl != null){
+                                this.$router.push(this.$route.params.nextUrl)
+                            }
+                            else {
+                                this.$router.push('dashboard')
+                            }
+                        }
+
                     })
                     .catch(function (error) {
                         console.error(error.response);
