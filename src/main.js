0dd836681bb00f4f698a683f6a4abe755eb8cf03
Mbugua KABAGE <mkabage@hotmail.com>
2019-11-21 15:23:31 +0530

    Added login logic

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import App from './components/App.vue'
 import Home from './components/Home.vue'
 import About from './components/About.vue'
 import Contact from './components/Contact.vue'
+import Dashboard from './components/Dashboard.vue'
+import Login from './components/Login.vue'
 
 Vue.config.productionTip = false
 
@@ -11,7 +13,9 @@ let router = new VueRouter({
   mode: 'history',
   routes: [ { path: '/', component: Home },
   { path: '/contact', component: Contact },
-  { path: '/about', component: About, }})
+  { path: '/about', component: About, },
+  { path: '/login', component: Login, meta: { guest: true } },
+  { path: '/dashboard', component: Dashboard, meta: { requiresAuth: true } } ] });
 
 Vue.use(VueRouter)
 
