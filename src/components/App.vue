d24155802ed662b6c4d54065340575bd679d5e43
mkabage <a102365@aup.edu>
2019-11-24 11:53:27 +0000

    typo

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    
+
 
     <img src="../assets/brand-logo.png" />
       <div>
     <nav class="container navbar navbar-expand-lg navbar-light bg-light">
-    <router-link to="/">Boquets</router-link>
+    <router-link to="/">Bouquets</router-link>
     <ul class="navbar-nav" style="width: 100%;">
 
-        
+
           <li class="nav-item">
             <router-link class="nav-link" to="/about">About us</router-link>
           </li>
           <li class="nav-item" style="margin-right: 490px">
             <router-link class="nav-link" to="/contact">Contact</router-link>
           </li>
-       
+
 
         <div class="col-md-4">
           <li class="nav-item right">
             <router-link class="nav-link" to="/help">Help</router-link>
-          </li>    
+          </li>
           <li class="nav-item right">
-            <a v-if="isLogin()" class="nav-link" @click="logoutBtn">Logout</a>            
+            <a v-if="isLogin()" class="nav-link" @click="logoutBtn">Logout</a>
             <router-link v-else class="nav-link" to="/login">Login</router-link>
           </li>
 
@@ -30,11 +30,11 @@
             <router-link class="nav-link" to="/dashboard">Cart</router-link>
           </li>
         </div>
-       
+
     </ul>
 
   </nav>
-</div> 
+</div>
 
   <hr/>
   <router-view></router-view>
@@ -58,17 +58,17 @@
           <router-link to="/contact">Contact us</router-link>
         </div>
       </div>
-      
+
 
       <div class="col-md-8 social-plugins" style="padding: 77px; text-align: center;">
-  
+
         <div>
           <font-awesome-icon :icon="['fab', 'twitter']" />
           <font-awesome-icon :icon="['fab', 'facebook']" />
           <font-awesome-icon :icon="['fab', 'pinterest']" />
           <font-awesome-icon :icon="['fab', 'instagram']" />
         </div>
-        
+
         <div>
         COPYRIGHT 2018 MISTER FLUERS
         </div>
