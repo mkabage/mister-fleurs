9262c99cb997acf6290c09a29cdaea2549a837b4
Mbugua Kabage <mkabage@gmail.com>
2019-11-24 09:15:00 +0100

    Cart changes vue direct

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,29 +1,36 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="contact-us">
+    <h4><u>Contact us</u></h4>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
 
-    <form
-      action="https://formspree.io/mdoorlge"
-      method="POST"
-    >
-      <label>
-        Your email:
-        <input type="text" name="_replyto">
-      </label>
-      <label>
-        Your message:
-        <textarea name="message"></textarea>
-      </label>
+    <div class="row">
+      <div class="col-md-4 offset-md-4">
+      <form
+        action="https://formspree.io/mdoorlge"
+        method="POST"
+      >
+      <div class="form-group">
+        <label>
+          Your email:
+          <input class="form-control" type="text" name="_replyto">
+        </label>
+        </div>
+      <div class="form-group">
+        <label>
+          Your message:
+          <textarea class="form-control" name="message"></textarea>
+        </label>
 
-      <!-- your other form fields go here -->
-
-      <button type="submit">Send</button>
-    </form>
+        <!-- your other form fields go here -->
+      </div>
+        <button type="submit" class="btn btn-primary">Send</button>  
+      </form>
+      </div>
+    </div>
   </div>
 </template>
 
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -13,34 +13,10 @@
       </div>
     </div>
   </div>
+
+  <CartItems />
   <hr/>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-4" style="margin-left: -50px">
-        <b>Select A Bouquet</b>
-      </div>
-      <div class="col-md-4">
-        <b style="background-color: black; padding: 5px 10px;color: pink;border-radius:20px;">Our BQTS</b>
-      </div>
-    </div>
-    <div class="row">
-    <div class="col-md-4" style="padding: 80px 80px 40px 80px;" v-for="product in this.products" :key="product.name" :name="product.name">
-      <img :src="product.img" style="width: 100%"/>
-      <div style="text-align: left; padding: 20px;">
-        <div> <b>Name:</b> {{product.name}}</div>
-        <div> <b>Price:</b> {{product.amount}}</div>
-        <div> <b>Delivery:</b> Free</div>
-        <div>
-          <a class="text-info" v-on:click="addToCart(product)"> Add</a>
-          <span style="margin: 0 15px;">{{productsCount[product.name] || 0}}</span>
-          <a class="text-info" v-if="invalidEmail" v-on:click="removeFromCart(product)"> Remove</a>
-        </div>
-      </div>
-    </div>
-  </div>
-  </div>
-  
-  <hr/>
+
   <div class="container">
     <div class="row">
       <div class="col-md-3">
@@ -68,69 +44,29 @@
     </div>
   </div>
 
-  <hr/>
-
-  <div class="container">
-    <div class="row">
-      <div class="col-md-4" style="margin-left: -50px">
-        <b>Select A Bouquet</b>
-      </div>
-      <div class="col-md-4">
-        <b style="background-color: black; padding: 5px 10px;color: pink;border-radius:20px;">Our BQTS</b>
-      </div>
-    </div>
-    <div class="row">
-    <div class="col-md-4" style="padding: 80px;" v-for="product in this.products" :key="product.name" :name="product.name">
-      <img :src="product.img" style="width: 100%"/>
-      <div style="text-align: left; padding: 20px;">
-        <div> <b>Name:</b> {{product.name}}</div>
-        <div> <b>Price:</b> {{product.amount}}</div>
-        <div> <b>Delivery:</b> Free</div>
-        <div>
-          <a class="text-info" v-on:click="addToCart(product)"> Add</a>
-          <span style="margin: 0 15px;">{{productsCount[product.name] || 0}}</span>
-          <a class="text-info" v-on:click="removeFromCart(product)"> Remove</a>
-        </div>
-      </div>
-    </div>
-  </div>
-  </div>
+  <CartItems />
 </div>
 </template>
 
 <script>
-import Product from './Product'
+import CartItems from './CartItems'
 
 export default {
   name: 'home',
   components: {
-    Product
+    CartItems: CartItems
   },
   data: function(){
     return {
-      products: [],
-      brandedProduct: {},
-      productsCount: {}
+      brandedProduct: {}
     }
   },
   created(){
     this.$http.get('https://api.myjson.com/bins/19pv12').then(res => {
-      this.products = res.data.slice(2);
       this.brandedProduct = res.data[4];
-      this.productsCount = this.recalculateCounts();
     })
   },
   components: {
-  },
-  methods: {
-    addToCart: function(item){
-      this.addItemsToCart(item);
-      this.productsCount = this.recalculateCounts();
-    },
-    removeFromCart: function(item){
-      this.removeItemsFromCart(item);
-      this.productsCount =  this.recalculateCounts();
-    }
   }
 }
 </script>
